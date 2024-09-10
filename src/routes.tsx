04cf03a6bd0aef53f4a1117931bb316f09cf4dd1
AmadeuGMeniconiDeveloper <amadevmeni@gmail.com>
2024-09-10 18:29:20 +0100

    React-router-dom and basic layout

---
+++ src/routes.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import { AppLayout } from "./pages/_layouts/app";
+import { Dashboard } from "./pages/app/dashboard";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { SignIn } from "./pages/auth/sign-in";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+    ],
+  },
+]);
