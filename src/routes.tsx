35d562ab46e3837566532215a155a610eba0e45b
AmadeuGMeniconiDeveloper <amadevmeni@gmail.com>
2024-09-11 23:40:22 +0100

    Basic 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import { AuthLayout } from "./pages/_layouts/auth";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
