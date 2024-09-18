4d664275b3508e9622678d92009c50dedbb69396
AmadeuGMeniconiDeveloper <amadevmeni@gmail.com>
2024-09-18 14:17:19 +0100

    Mock Api setup with MSW

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const evnSchema = z.object({
+  MODE: z.enum(["development", "production", "test"]),
   VITE_API_URL: z.string().url(),
   VITE_ENABLE_API_REQUEST_THROTTLE: z
     .string()
